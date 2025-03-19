f907c4721a954744521891367ed3be19f90f2a68
mooenz <joss92821@hotmail.com>
2025-03-18 21:20:28 -0500

    Agregar funcionalidad de navegación por tabs en la sección de documentación y crear archivo JS minificado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,24 @@
 const windowIsReady = window.addEventListener('DOMContentLoaded', () => {
-	const $ = (selector) => document.querySelector(selector);
-	console.log('Window is ready');
+	const $ = (selector) => document.querySelectorAll(selector);
+
+	// Navegación tabs
+	const tabs = $('.documentacion__tabs .documentacion__item ');
+	const tabContents = $('.documentacion__informacion .documentacion__informacion--item');
+
+	tabs.forEach((tab) => {
+		tab.addEventListener('click', (e) => {
+			const tab = e.target;
+			const tabContent = tab.dataset.content;
+			const tabContentElement = document.querySelector(tabContent);
+
+			tabs.forEach((tab) => tab.classList.remove('activo'));
+			tab.classList.add('activo');
+
+			tabContents.forEach((content) => {
+				content.classList.remove('activo');
+			});
+
+			tabContentElement.classList.add('activo');
+		});
+	});
 });
