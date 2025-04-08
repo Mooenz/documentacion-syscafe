39a2327071bf3aea54c910aa7b50844d47479eb0
mooenz <joss92821@hotmail.com>
2025-04-07 20:48:52 -0500

    Agregar funcionalidad para evitar el comportamiento por defecto en la navegación por tabs y actualizar el año en la sección de documentación

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,19 @@ const windowIsReady = window.addEventListener('DOMContentLoaded', () => {
 
 	tabs.forEach((tab) => {
 		tab.addEventListener('click', (e) => {
+			// Evitar el comportamiento por defecto del enlace
+			e.preventDefault();
+
+			// Obtener el contenido del tab y el elemento correspondiente
 			const tab = e.target;
 			const tabContent = tab.dataset.content;
 			const tabContentElement = document.querySelector(tabContent);
 
+			// Remover la clase 'activo' de todos los tabs y agregarla al tab actual
 			tabs.forEach((tab) => tab.classList.remove('activo'));
 			tab.classList.add('activo');
 
+			// Remover la clase 'activo' de todos los contenidos y agregarla al contenido actual
 			tabContents.forEach((content) => {
 				content.classList.remove('activo');
 			});
@@ -21,4 +27,9 @@ const windowIsReady = window.addEventListener('DOMContentLoaded', () => {
 			tabContentElement.classList.add('activo');
 		});
 	});
+
+	// Date
+	const year = new Date().getFullYear();
+	const yearElement = document.querySelector('#fecha');
+	yearElement.innerHTML = year;
 });
