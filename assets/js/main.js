841b5a015130addd7f8a81d50526329d627854ad
mooenz <joss92821@hotmail.com>
2025-04-28 22:54:33 -0500

    Refactor code structure for improved readability and maintainability

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,73 @@ const windowIsReady = window.addEventListener('DOMContentLoaded', () => {
 		});
 	});
 
+	// Navegación interna
+	const tabsInternos = $('.documentacion__informacion--contenedor li');
+	tabsInternos.forEach((tab) => {
+		tab.addEventListener('click', (e) => {
+			tabsInternos.forEach((tab) => tab.classList.remove('activo'));
+			tab.classList.add('activo');
+		});
+	});
+
+	// Cuando Documentación esté en la pantalla, agregamos el activo al link flotante
+	const documentacion = document.querySelector('.documentacion');
+	const documentacionLink = document.querySelector('.link-encuesta');
+
+	const observer = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					setTimeout(() => {
+						documentacionLink.classList.add('activo');
+					}, 5000);
+
+					observer.unobserve(documentacion);
+				}
+			});
+		},
+		{
+			threshold: 0.2,
+		}
+	);
+
+	observer.observe(documentacion);
+
+	// Botón de cerrar el enlace flotante
+	const closeButton = document.querySelector('#link-encuesta-cerrar');
+
+	closeButton.addEventListener('click', () => {
+		documentacionLink.classList.remove('activo');
+	});
+
+	// Botón de volver al inicio de la documentación
+	const backToTopButton = document.querySelector('#volver-arriba');
+	backToTopButton.addEventListener('click', () => {
+		documentacion.scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		});
+	});
+
+	// Si el usuario esta en el inicio de la documentación, ocultamos el botón de volver al inicio
+	const inicio = document.querySelector('#hero');
+	const observer2 = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					backToTopButton.classList.remove('activo');
+				} else {
+					backToTopButton.classList.add('activo');
+				}
+			});
+		},
+		{
+			threshold: 0.2,
+		}
+	);
+
+	observer2.observe(inicio);
+
 	// Date
 	const year = new Date().getFullYear();
 	const yearElement = document.querySelector('#fecha');
